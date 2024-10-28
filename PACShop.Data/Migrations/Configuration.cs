a5d096e0cfb7f2325be35720534389c0b88a5ea6
DESKTOP-341FT42\Admin <huynd8702@gmail.com>
2024-10-28 10:30:12 +0700

    add code first

---
+++ PACShop.Data/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace PACShop.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PACShop.Data.PACShopDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PACShop.Data.PACShopDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}

4960cda571e75ba9c252f2730a778bd02228ead0
DESKTOP-341FT42\Admin <huynd8702@gmail.com>
2024-11-05 10:22:17 +0700

    Bai22

--- PACShop.Web/app/app.js
+++ PACShop.Web/app/app.js
@@ -1,1 +1,18 @@
-﻿
+﻿/// <reference path="../assets/admin/libs/angular/angular.js" />
+
+(function () {
+    angular.module('pacshop', ['pacshop.products', 'pacshop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            //parent: 'base',
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
---
+++ PACShop.Web/app/shared/modules/pacshop.common.js
@@ -0,0 +1,3 @@
+﻿(function () {
+    angular.module('pacshop.common',['ui.router'])
+})();
